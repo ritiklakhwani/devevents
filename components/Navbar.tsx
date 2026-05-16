c75ac04aeb499737e4eeff1a72ce8591a10faea5
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-05-16 16:21:09 +0530

    feat: done with the basic nav bar and styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <nav>
         <Link href="/" className="logo">
-          <Image src="icons/logo.png" alt="logo" width={24} height={24} />
+          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
 
           <p>DevEvent</p>
         </Link>
